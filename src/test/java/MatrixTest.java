94a419dbd550cfe29dcb26eff7d24469eda8d6b4
Tuomo Kriikkula <tuomo.kriikkula@student.oulu.fi>
2017-05-13 20:33:07 +0300

    Initialize Integer Matrices for tests

--- src/test/java/MatrixTest.java
+++ src/test/java/MatrixTest.java
@@ -2,6 +2,9 @@ import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class MatrixTest extends TestCase {
 
     public MatrixTest(String testName) {
@@ -18,11 +21,25 @@ public class MatrixTest extends TestCase {
 
     public void testTranspose() throws Exception {
 
-        Matrix<Integer> matrix123 = new Matrix<Integer>();
-        Matrix<Integer> matrix321 = new Matrix<Integer>();
+        ArrayList<ArrayList<Integer>> data1 = new ArrayList<ArrayList<Integer>>();
+        Integer[] arr1 = {1, 2, 3};
+        data1.add(new ArrayList<Integer>(Arrays.asList(arr1)));
+        data1.add(new ArrayList<Integer>(Arrays.asList(arr1)));
+        data1.add(new ArrayList<Integer>(Arrays.asList(arr1)));
 
-        assertEquals(true, true);
-        assertEquals(true, true);
+        ArrayList<ArrayList<Integer>> data2 = new ArrayList<ArrayList<Integer>>();
+        Integer[] arr2 = {1, 1, 1};
+        Integer[] arr3 = {2, 2, 2};
+        Integer[] arr4 = {3, 3, 3};
+        data2.add(new ArrayList<Integer>(Arrays.asList(arr2)));
+        data2.add(new ArrayList<Integer>(Arrays.asList(arr3)));
+        data2.add(new ArrayList<Integer>(Arrays.asList(arr4)));
+
+        Matrix<Integer> matrix1 = new Matrix<Integer>(data1);
+        Matrix<Integer> matrix2 = new Matrix<Integer>(data2);
+
+        assertEquals(matrix1, matrix2.transpose());
+        assertEquals(matrix2, matrix1.transpose());
 
     }
 }
